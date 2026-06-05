69b0b3b5ebd53ce99d31c71e9328c0a719a96441
EffessDev <faseeh1080@gmail.com>
2026-06-05 09:57:04 +0530

    remove discover route

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"
 
 export default async function Page() {
+  const session = await auth.api.getSession({ headers: await headers() })
+  if (session !== null) {
+    return (
+      <div>
+        <p>Welcome, {session.user.name}!</p>
+      </div>
+    )
+  }
+
   return (
     <div>
       <p>Landing Page</p>
