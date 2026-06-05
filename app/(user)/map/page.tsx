df4fbb31a7548cd00155579ef75633bc4ffff937
EffessDev <faseeh1080@gmail.com>
2026-06-05 16:57:48 +0530

    style: maps coming soon

--- app/(user)/map/page.tsx
+++ app/(user)/map/page.tsx
@@ -1,3 +1,7 @@
 export default function MapPage() {
-  return <p>Coming soon</p>
+  return (
+    <div className="flex h-[75vh] w-full items-center justify-center">
+      <p className="mx-auto text-center">Coming soon...</p>
+    </div>
+  )
 }
