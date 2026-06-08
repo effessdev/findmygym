ff6bde0876f2648297796cb3b0c700e0ba11b3f7
EffessDev <faseeh1080@gmail.com>
2026-06-08 19:37:28 +0530

    style me page

--- app/(main)/me/page.tsx
+++ app/(main)/me/page.tsx
@@ -6,6 +6,15 @@ import Link from "next/link"
 import db from "@/db/db"
 import { gym } from "@/db/schema/gym-schema"
 import { eq, count } from "drizzle-orm"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default async function MePage() {
   const session = await auth.api.getSession({ headers: await headers() })
@@ -25,16 +34,36 @@ export default async function MePage() {
     <div>
       <div className="flex flex-col gap-4 px-4 py-8">
         <h1 className="text-4xl font-bold">Hello, {session.user.name}</h1>
-        <p className="text-muted-foreground">
-          Here, you&apos;ll manage your memberships, your profile, and more.
-        </p>
-        <Link href="/partner">
-          <Button>
-            {gymCount > 0
-              ? "Go to Partnership Dashboard"
-              : "Join the Partner Program"}
-          </Button>
-        </Link>
+        <p className="text-muted-foreground">Manage your profile here.</p>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Your Memberships</CardTitle>
+            <CardDescription>View and manage your memberships.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>You don&apos;t have any memberships yet.</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Partnership Program</CardTitle>
+            <CardDescription>
+              Join our partnership program to display your gym in our apps. We
+              will handle discovery and payment for you!
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Link href="/partner">
+              <Button>
+                {gymCount > 0
+                  ? "Go to Partnership Dashboard"
+                  : "Join the Partnership Program"}
+              </Button>
+            </Link>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
